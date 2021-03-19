d30cb7e2813705d6bfc5ba125d79a317ea5ab0fb
yann <yann.boisselier@gmail.com>
2021-03-19 18:23:15 +0100

    remove saas module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StripeModule } from './stripe/stripe.module';
 import { ConfigModule } from '@nestjs/config';
-import { TestModule } from './test/test.module';
 import stripeConfig from './stripe/stripe.config';
 
 @Module({
