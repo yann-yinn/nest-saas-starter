ba76ea4ff0021075f704b42fb7643c549d1c5807
yann <yann.boisselier@gmail.com>
2021-03-19 19:33:01 +0100

    fix error handling and validation for stripe controllers

--- src/stripe/stripe.controller.ts
+++ src/stripe/stripe.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, Res, Post, Body } from '@nestjs/common';
+import { Controller, Get, Req, Res, Post, Body, HttpException, HttpStatus } from '@nestjs/common';
 import { StripeService } from './stripe.service';
 import { AdapterService } from './adapter.service';
 import { ConfigService } from '@nestjs/config';
@@ -10,85 +10,79 @@ import { createCheckoutSessionDto } from "./dto";
 export class StripeController {
   constructor(private readonly stripeService: StripeService, private configService: ConfigService,  private adapterService: AdapterService) {}
 
+  /**
+   * Récupérer la liste des plans depuis Stripe.
+   */
   @Get('plans')
   getPlans() {
     return this.stripeService.getPlans();
   }
 
+  /**
+   * Commence une nouvelle session d'achat. Retourne un sessionId
+   * qui pourra être utilisé par le client pour redirigé vers Stripe Checkout. 
+   */
   @Post("create-checkout-session")
   async createCheckoutSession(@Res() res: Response, @Req() req: Request, @Body() createCheckoutSessionDto: createCheckoutSessionDto) {
     const priceId: string = req.body.priceId;
-    
-    try {
-      let checkoutConfig: Stripe.Checkout.SessionCreateParams = {
-        mode: "subscription",
-        payment_method_types: ["card"],
-        line_items: [
-          {
-            price: priceId,
-            // For metered billing, do not pass quantity
-            quantity: 1,
-          },
-        ],
-        // en cas de succès du paiement, le visiteur sera redirigé à cette adresse:
-        success_url: this.configService.get('stripeCheckoutSuccessUrl'),
-        // en cas d'annulation du paiement, rediriger le visiteur à cette adresse:
-        cancel_url: this.configService.get('stripeCheckoutCancelUrl'),
-      };
-      
-      await this.adapterService.onCreateCheckoutSession({
-        req,
-        checkoutConfig,
-        priceId,
-      });
-  
-      const session = await this.stripeService.getStripe().checkout.sessions.create(checkoutConfig);
-      res.send({
-        sessionId: session.id,
-      });
-    } catch (e) {
-      res.status(400);
-      return res.send({
-        error: {
-          message: e.message,
+    let checkoutConfig: Stripe.Checkout.SessionCreateParams = {
+      mode: "subscription",
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price: priceId,
+          // For metered billing, do not pass quantity
+          quantity: 1,
         },
-      });
-    }
+      ],
+      // en cas de succès du paiement, le visiteur sera redirigé à cette adresse:
+      success_url: this.configService.get('stripeCheckoutSuccessUrl'),
+      // en cas d'annulation du paiement, rediriger le visiteur à cette adresse:
+      cancel_url: this.configService.get('stripeCheckoutCancelUrl'),
+    };
+    
+    await this.adapterService.onCreateCheckoutSession({
+      req,
+      checkoutConfig,
+      priceId,
+    });
+
+    const session = await this.stripeService.getStripe().checkout.sessions.create(checkoutConfig);
+    return { sessionId: session.id }
   }
 
-  @Get('webhooks')
+  /**
+   * Stripe enverra des données sur ce chemin lors d'évènement majeurs
+   * comme un abonnement acheté avec succès.
+   */
+  @Post('webhooks')
   async webhooks(@Res() res: Response, @Req() req: Request) {
 
-    const signature = req.headers["stripe-signature"];
     if (!this.configService.get('stripeWebhookSecret')) {
-      res.status(400);
-      res.send({
-        error: {
-          message: "config.stripeWebhookSecret is not defined",
-        },
-      });
-      return;
+      throw new HttpException({
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: 'process.env.stripeWebhookSecret is not defined.',
+      }, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
     try {
       const event = this.stripeService.getStripe().webhooks.constructEvent(
         req.body,
-        signature,
+        req.headers["stripe-signature"],
         this.configService.get('stripeWebhookSecret')
       );
       await this.adapterService.onWebhooks({ req, event });
-      return { status: "ok" }
-    } catch (e) {
-      console.log("e", e);
-      res.status(400).send({
-        error: {
-          message: e.message,
-        },
-      });
-      return;
+      return { status: "ok" };
+    } 
+    catch (error) {
+      throw new HttpException({
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: error.message,
+      }, HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 
-  @Get('create-customer-portal-session')
+  @Post('create-customer-portal-session')
   async createCustomerPortalSession(@Res() res: Response, @Req() req: Request) {
 
     const portalConfig: Stripe.BillingPortal.SessionCreateParams = {
@@ -99,25 +93,22 @@ export class StripeController {
     await this.adapterService.onCreateCustomerPortalSession({ req, portalConfig });
 
     if (!portalConfig.customer) {
-      res.status(400).send({
-        error: {
-          message: 'Error: "customerId" is required. Received: ' + portalConfig.customer,
-        },
-      });
-    } else {
-      try {
-        const portalsession = await this.stripeService.getStripe().billingPortal.sessions.create(portalConfig);
-        res.send({
-          url: portalsession.url,
-        });
-      } catch (e) {
-        res.status(400);
-        res.send({
-          error: {
-            message: e.message,
-          },
-        });
-      }
+      throw new HttpException({
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: '"customerId" is missing: ' + portalConfig.customer,
+      }, HttpStatus.INTERNAL_SERVER_ERROR);
+    } 
+
+    try {
+      const portalsession = await this.stripeService.getStripe().billingPortal.sessions.create(portalConfig);
+      return { url: portalsession.url };
+    } 
+    catch (error) {
+      throw new HttpException({
+        statusCode: HttpStatus.INTERNAL_SERVER_ERROR,
+        message: error.message,
+      }, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
   }
 }
