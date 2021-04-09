1152b6dccb7bb748d386be5412616685e3904c2c
yann <yann.boisselier@gmail.com>
2021-04-09 05:22:59 +0200

    add comments for cookies

--- src/auth-session/auth-session.bootstrap.ts
+++ src/auth-session/auth-session.bootstrap.ts
@@ -3,8 +3,6 @@ import * as connectMongodbSession from 'connect-mongodb-session';
 const MongoDBStore = connectMongodbSession(session);
 import { INestApplication } from '@nestjs/common';
 
-console.log('rocess.env.AUTH_SESSION_SECRET', process.env.NODE_ENV);
-
 /**
  * To be called in ./src/main.ts
  */
@@ -16,6 +14,7 @@ export function authSessionBootstrap(app: INestApplication): void {
       // provide an array of secrets, with the new secret as first element of the array,
       // and including previous secrets as the later elements.
       secret: [<string>process.env.AUTH_SESSION_SECRET],
+      // note sure about this one.
       resave: false,
       cookie: {
         path: '/',
@@ -27,6 +26,8 @@ export function authSessionBootstrap(app: INestApplication): void {
         maxAge: 24 * 60 * 60 * 1000,
         // for which domains cookie will be automatically sent
         domain: process.env.DOMAIN,
+        // Most secured option is "strict". But "lax" still mitigates some of CSRF attacks
+        // because it does *not* send cookie on POST cross-origins request.
         sameSite: 'strict',
       },
       saveUninitialized: false, // do not save if req.session is an empty object {}
