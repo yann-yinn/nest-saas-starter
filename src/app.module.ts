bba05c30966e306411491587cab770b505bc6eb4
yann <yann.boisselier@gmail.com>
2021-04-10 23:38:17 +0200

    add typeorm with postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,12 +9,29 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AuthJwtModule } from './auth-jwt/auth-jwt.module';
 import { ConfigService } from '@nestjs/config';
 import { AuthSessionModule } from './auth-session/auth-session.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       load: [appConfig],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(<string>process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      entities: [],
+      synchronize: true,
+      ssl: true,
+      extra: {
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
     }),
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
