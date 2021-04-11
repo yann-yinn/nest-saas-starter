128554d77e63fd866ef0926cd8b6bb4cb1d681f6
yann <yann.boisselier@gmail.com>
2021-04-11 02:27:37 +0200

    remove old code

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { authSessionBootstrap } from './auth-session/auth-session.bootstrap';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  authSessionBootstrap(app);
   await app.listen(3000);
 }
 
